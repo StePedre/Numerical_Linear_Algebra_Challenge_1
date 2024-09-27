34cf2273f22da75c0c672445a15eb324c0c21d41
angelonotarnicola <anotarnicola@soldo.com>
2024-09-27 11:07:02 +0200

    Inserted main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <Eigen/Dense>
+#include <iostream>
+#include <cstdlib>
+
+// from https://github.com/nothings/stb/tree/master
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+using namespace Eigen;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    
+}
